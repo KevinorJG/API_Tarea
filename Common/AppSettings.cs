ecbdf1d72f18e8de66ff7a7cb3d37f7644202ee7
Kevin Ortiz <kevinjair2003@gmail.com>
2022-04-26 14:11:10 -0600

    Agregar archivos y cambios para el funcionamiento

--- Common/AppSettings.cs
+++ Common/AppSettings.cs
@@ -14,5 +14,7 @@ namespace Common
         public static string ApiUrl { get => ConfigurationManager.AppSettings.Get("ApiUrl"); }
         public static string Token { get => ConfigurationManager.AppSettings.Get("Token"); }
         public static string ApiIcons { get => ConfigurationManager.AppSettings.Get("ApiIcons"); }
+
+
     }
 }
